bdd41a575c5a978fb269e102c85ba999ee92fd6b
setyazero1 <setya@LAPTOP-0URGS4JM.localdomain>
2025-10-15 04:16:30 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Tambahkan konfigurasi lainnya di sini jika ada
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true,
+      topLevelAwait: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
